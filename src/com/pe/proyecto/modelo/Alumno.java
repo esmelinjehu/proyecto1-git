156063e35f21e916e7a16004ec0b9434d2de9b0e
esmelinjehu <carlh_12@hotmail.com>
2017-06-08 21:41:21 -0500

    se creo clase alumno

---
+++ src/com/pe/proyecto/modelo/Alumno.java
@@ -0,0 +1,21 @@
+package com.pe.proyecto.modelo;
+
+public class Alumno {
+
+	private String nombre;
+
+	public Alumno(){
+		
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	
+}
